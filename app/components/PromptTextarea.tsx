21c4ce1ab1a99ec960a10a7212608e4ee0009ff6
Olya Vo <olga.volkova@email.cz>
2025-11-06 14:57:03 +0100

    fix onChange confirm

--- app/components/PromptTextarea.tsx
+++ app/components/PromptTextarea.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useRef, type ChangeEvent } from "react";
+import { FocusEvent, useState, type ChangeEvent } from "react";
 
 interface PromptTextareaProps {
   label: string;
@@ -11,51 +11,44 @@ interface PromptTextareaProps {
   className?: string;
 }
 
-export default function PromptTextarea({
-  label,
-  value,
-  onChange,
-  restartChats,
-  placeholder,
-}: PromptTextareaProps) {
-  const editingActive = useRef(false);
+export function PromptTextarea({ label, value, onChange, restartChats, placeholder }: PromptTextareaProps) {
+  const [initialValue, setInitialValue] = useState("");
 
   const handleBeforeChange = () => {
-    if (!editingActive.current) {
-      const confirmed = window.confirm(
-        "Are you sure you want to restart all the chats?"
-      );
-      if (!confirmed) return false;
-      editingActive.current = true;
-      restartChats();
-    }
+    const confirmed = window.confirm(
+      "If you modify the prompt, the current conversation will be cleared. Are you sure you want to restart the chat?"
+    );
+    if (!confirmed) return false;
+    restartChats();
     return true;
   };
 
   const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
-    const nextValue = e.target.value;
-    const shouldProceed = handleBeforeChange();
-
-    // Store the value that should be displayed in the ::after pseudo-element
-    // (for textarea height to be unlimited)
+    // Store the value that should be displayed in the ::after pseudo-element (for the textarea height to be unlimited)
     if (e.target.parentElement) {
-      e.target.parentElement.dataset.clonedVal = !shouldProceed
-        ? value
-        : nextValue;
+      e.target.parentElement.dataset.clonedVal = e.target.value;
     }
-    if (shouldProceed === false) {
-      // Restore the displayed value
-      e.target.value = value;
-      return;
+  };
+  const handleFocus = (e: FocusEvent) => {
+    const textarea = e.target as HTMLTextAreaElement;
+    setInitialValue(textarea.value);
+  };
+  const handleBlur = (e: FocusEvent) => {
+    const textarea = e.target as HTMLTextAreaElement;
+    if (textarea.value !== initialValue) {
+      const shouldProceed = handleBeforeChange();
+      if (shouldProceed === false) {
+        // Restore the displayed value
+        textarea.value = initialValue;
+      } else {
+        onChange(textarea.value);
+      }
     }
-    onChange(nextValue);
   };
 
   return (
     <label className="flex w-full flex-col gap-2">
-      <div className="text-sm font-medium text-zinc-700 dark:text-zinc-300 text-right">
-        {label}
-      </div>
+      <div className="text-sm font-medium text-zinc-700 dark:text-zinc-300 text-right">{label}</div>
       <div
         className={`
         grid
@@ -76,10 +69,11 @@ export default function PromptTextarea({
       >
         <textarea
           className="min-h-10 w-full rounded-md border border-zinc-200 p-2 text-sm outline-none focus:ring-2 focus:ring-sky-300 dark:border-zinc-800 dark:bg-transparent"
-          value={value}
+          defaultValue={value}
           placeholder={placeholder}
           onChange={handleChange}
-          onBlur={() => (editingActive.current = false)}
+          onBlur={handleBlur}
+          onFocus={handleFocus}
         />
       </div>
     </label>
